26e27f156a57a96206ff49d40c8bc802089add98
Domenic Murtari <domenic.murtari@gmail.com>
2014-02-23 11:46:50 -0700

    Added multithreaded program and header (currently duplicate of single threaded implementation)

---
+++ multi-lookup.h
@@ -0,0 +1,17 @@
+#ifndef MULTI-LOOKUP_H
+#define MULTI-LOOKUP_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include "util.h"
+#include "queue.h"
+
+#define MINARGS 3
+#define USAGE "<inputFilePath> <outputFilePath>"
+#define SBUFSIZE 1025
+#define INPUTFS "%1024s"
+
+#endif
