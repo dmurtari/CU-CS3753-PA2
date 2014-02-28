efcfa7f97caf4a927b95c02cad6af4d271a6ec8e
Domenic Murtari <domenic.murtari@gmail.com>
2014-02-27 22:42:13 -0700

    Resolved merge conflict

--- multi-lookup.c
+++ multi-lookup.c
@@ -151,11 +151,7 @@ int main(int argc, char* argv[]){
   /* Number of requester threads is number of input files */
   int requesterThreadCount = argc - 2;
   /* Number of resolver threads is the number of cores */
-<<<<<<< HEAD
-  int resolverThreadCount = sysconf( _SC_NPROCESSORS_ONLN );
-=======
-  int resolverThreadCount = sysconf(_SC_NPROCESSORS_ONLN) * 2;
->>>>>>> 575174d751012770fc53a406e30f76c80daec1e1
+  int resolverThreadCount = sysconf( _SC_NPROCESSORS_ONLN ) * 2;
   /* Set number of running requesters to the numbers of input files */
   runningRequesters = requesterThreadCount;
 
